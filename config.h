d1faaf00c37edc94f834da1281d43a6513b27764
banana <jgjin@umich.edu>
2020-05-23 16:55:48 -0400

    config

---
+++ config.h
@@ -0,0 +1,18 @@
+// configuration for jgjin's crkbd keymap
+
+#pragma once
+
+// left side is master
+#define MASTER_LEFT
+
+// use OLED screens
+#define SSD1306OLED
+
+// def for split keyboard?
+#define USE_SERIAL_PD2
+
+// https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#permissive-hold
+#define PERMISSIVE_HOLD
+
+// threshold (in ms) to interpret tap rather than hold
+#define TAPPING_TERM 200
