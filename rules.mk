db6c845db4c73b877908f275f413dccae7c9b6e0
banana <jgjin@umich.edu>
2020-05-23 16:55:51 -0400

    rules

---
+++ rules.mk
@@ -0,0 +1,10 @@
+
+# key settings
+TAP_DANCE_ENABLE=yes
+UNICODEMAP_ENABLE = yes
+
+# OLED display settings
+SRC +=  ./lib/glcdfont.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        ./lib/keylogger.c \
