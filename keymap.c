e61619162b267b83f657bc49a57b6f3a734a4c86
Jason Jin <jgjin@umich.edu>
2020-05-24 02:36:12 -0400

    smol syntax fix

--- keymap.c
+++ keymap.c
@@ -231,7 +231,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
         }
         return false;
-    }
     case QUOTE:
         if (get_mods() & MOD_MASK_SHIFT) {
             if (record->event.pressed) {
